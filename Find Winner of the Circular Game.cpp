085dd40a8cce80a97271c8a462c2da50b9dafa3f
Amanjot Singh Saini <116896075+AmanjotSingh0908@users.noreply.github.com>
2025-01-01 23:41:12 +0530

    Update Find Winner of the Circular Game.cpp

--- Find Winner of the Circular Game.cpp
+++ Find Winner of the Circular Game.cpp
@@ -10,3 +10,15 @@ public:
         return findAnswer(n,k) + 1;
     }
 };
+
+//iterative approach
+class Solution {
+public:
+    int findTheWinner(int n, int k) {
+        int winner = 0;
+        for(int i = 2; i<= n; i++){
+            winner = (winner + k) % i;
+        }
+        return winner + 1;
+    }
+};
